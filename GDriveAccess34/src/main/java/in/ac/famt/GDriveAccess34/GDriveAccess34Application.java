e9606e04d007d7a4af0b5b49064660aa2b2e9e01
Onkar Malawade <132775903+OnkarMalawade@users.noreply.github.com>
2024-11-23 22:20:56 +0530

    Add files via upload

---
+++ GDriveAccess34/src/main/java/in/ac/famt/GDriveAccess34/GDriveAccess34Application.java
@@ -0,0 +1,13 @@
+package in.ac.famt.GDriveAccess34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GDriveAccess34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GDriveAccess34Application.class, args);
+	}
+
+}
